b5f212bcb717a7ab2e4ae1767dd8b9625c82f095
HenriquePNZ <h254260@dac.unicamp.br>
2025-01-21 20:33:42 -0300

    Adicionando dinamização de nomes de imagens, implementação do banco de dados SQLite e criação da primeira model utilizando o ORM do Django

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Fotografia(models.Model):
+  
+   #inicia o campo nome com o tipo char, limita o numero maximo de caracteres a 100, não permite que seja null e nem blank (x = "")
+    nome = models.CharField(max_length = 100, null = False, blank = False)
+    legenda = models.CharField(max_length = 150, null = False, blank = False)
+    descricao = models.TextField(null = False, blank = False)
+    #caminho da foto
+    foto = models.CharField(max_length = 100, null = False, blank = False)
+    
+    def ___str__(self):
+        return f"Fotografia [nome= {self.nome} ]"

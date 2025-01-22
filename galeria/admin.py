64a2ce7f8646f0cc1d2923b6a6677209d41ec105
HenriquePNZ <h254260@dac.unicamp.br>
2025-01-22 10:39:54 -0300

    Atualizan templates para acessar dados do banco, criando superuser, adicionando tabela Fotografia ao Django Admin e personalizando a interface administrativa.

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
 # Register your models here.
+
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda")
+    list_display_links = ("id", "nome") #Permite que eu clique também no campo nome para acessar os dados do objeto (na plataforma do admin)
+    search_fields = ("nome",) # Adiciona um campo de busca na plataforma do admin para que eu consiga encontrar objetos com maior facilidade
+admin.site.register(Fotografia, ListandoFotografias)
--- galeria/models.py
+++ galeria/models.py
@@ -2,9 +2,18 @@ from django.db import models
 
 class Fotografia(models.Model):
   
+    #Cria uma lista de tuplas com as categorias já existentes no site para evitar que sejam criadas outras categorias que não devem existir
+    OPCOES_CATEGORIA = [
+      ("NEBULOSA", "Nebulosa"),
+      ("ESTRELA", "Estrela"),
+      ("GALÁXIA", "Galáxia"),
+      ("PLANETA", "Planeta")
+    ]
+  
    #inicia o campo nome com o tipo char, limita o numero maximo de caracteres a 100, não permite que seja null e nem blank (x = "")
     nome = models.CharField(max_length = 100, null = False, blank = False)
     legenda = models.CharField(max_length = 150, null = False, blank = False)
+    categoria = models.CharField(max_length = 100, choices = OPCOES_CATEGORIA, default = '')
     descricao = models.TextField(null = False, blank = False)
     #caminho da foto
     foto = models.CharField(max_length = 100, null = False, blank = False)

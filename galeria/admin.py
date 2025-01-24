1840e0abd37cdb7d6f0d611ce07fd69ab48b425d
HenriquePNZ <h254260@dac.unicamp.br>
2025-01-24 12:54:12 -0300

    Adicionando campos de data e status à model Fotografia, implementando funcionalidades de publicação e ordenação por mais recentes.

--- galeria/admin.py
+++ galeria/admin.py
@@ -4,7 +4,10 @@ from galeria.models import Fotografia
 # Register your models here.
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ("id", "nome", "legenda")
+    list_display = ("id", "nome", "legenda", "publicada")
     list_display_links = ("id", "nome") #Permite que eu clique também no campo nome para acessar os dados do objeto (na plataforma do admin)
     search_fields = ("nome",) # Adiciona um campo de busca na plataforma do admin para que eu consiga encontrar objetos com maior facilidade
+    list_filter= ("categoria",)
+    list_editable = ("publicada",)
+    list_per_page = 10 #Limita o numero de objetos a 10 por página
 admin.site.register(Fotografia, ListandoFotografias)
--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import models
 
 class Fotografia(models.Model):
@@ -17,6 +18,8 @@ class Fotografia(models.Model):
     descricao = models.TextField(null = False, blank = False)
     #caminho da foto
     foto = models.CharField(max_length = 100, null = False, blank = False)
+    publicada = models.BooleanField(default = False)
+    data_fotografia = models.DateTimeField(default=datetime.now, blank = False)
     
     def ___str__(self):
         return f"Fotografia [nome= {self.nome} ]"

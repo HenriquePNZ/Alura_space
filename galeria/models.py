0af9c421641ea760b1eac54aec08deadd6d5d8c0
HenriquePNZ <h254260@dac.unicamp.br>
2025-02-03 15:29:43 -0300

    Adicionando funcionalidade de busca com nova URL, view e template, incluindo queries para filtrar fotografias.

--- galeria/models.py
+++ galeria/models.py
@@ -17,9 +17,9 @@ class Fotografia(models.Model):
     categoria = models.CharField(max_length = 100, choices = OPCOES_CATEGORIA, default = '')
     descricao = models.TextField(null = False, blank = False)
     #caminho da foto
-    foto = models.CharField(max_length = 100, null = False, blank = False)
+    foto = models.ImageField(upload_to = "fotos/%Y/%m/%d/", blank = True)
     publicada = models.BooleanField(default = False)
     data_fotografia = models.DateTimeField(default=datetime.now, blank = False)
     
     def ___str__(self):
-        return f"Fotografia [nome= {self.nome} ]"
+        return self.nome
